8450c0f258c8bd84c7b1cfca4aed29e710abdebd
developersabhi <chaturvediabhi111@gmail.com>
2024-11-20 12:56:36 +0530

    register validation check with the help of postman

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
-const register = (req, res) => {
+const { validationResult } = require('express-validator')
 
+
+const register = (req, res) => {
+    const errors = validationResult(req);
+    console.log("register controller")
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
 }
 
 module.exports = { register }
